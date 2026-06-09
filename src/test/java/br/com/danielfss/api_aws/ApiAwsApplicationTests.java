cbcd92810f8110fda2e0fb0abcd0aeabc95feb8d
Daniel Souza <daniel.fer.ss@hotmail.com>
2026-06-09 20:00:34 -0300

    commit inicial

---
+++ src/test/java/br/com/danielfss/api_aws/ApiAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danielfss.api_aws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
